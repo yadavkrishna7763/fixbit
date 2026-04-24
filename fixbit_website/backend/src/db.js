9c06b6850b3ddf2225e5d9f302f0706f1df135fd
Krishna Yadav <krishnayadav7763@gmail.com>
2026-04-25 04:16:21 +0530

    Enhance database connection with SSL settings
    Added SSL for TiDB connection

--- fixbit_website/backend/src/db.js
+++ fixbit_website/backend/src/db.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql2');
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
 }).promise();
 
 module.exports = db;
